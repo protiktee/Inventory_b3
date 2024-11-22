d6a7e1b16498355ce584a2d8f0015f8150af3c9d
Md. Nafiur Rahman Protik <protiktee@users.noreply.github.com>
2024-11-22 09:40:53 +0600

    Initial commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory_b3.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        { 
+            if (username == "admin" && password == "123")
+            { 
+                Session["username"] = username;
+                ViewBag.Msg = "Login Success";
+            }
+            else
+            {
+                ViewBag.Msg = "Login Failed";
+            }
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            Session.Remove("username");
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Inventory_b3.MvcApplication" Language="C#" %>
